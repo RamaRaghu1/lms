4b34835ac853450c737dcee61141a327678de49d
Rama Raghu <ramanadar003@gmail.com>
2024-06-09 18:18:56 +0530

    commit changes

--- client/src/components/Admin/Course/CourseContent.js
+++ client/src/components/Admin/Course/CourseContent.js
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import { AiOutlineDelete } from "react-icons/ai";
 import { MdOutlineKeyboardArrowDown } from "react-icons/md";
-import {styles} from "../../../styles/style"
-import {BsPencil} from "react-icons/bs";
-
-
+import { styles } from "../../../styles/style";
+import { BsPencil } from "react-icons/bs";
 
 const CourseContent = ({
   courseContentData,
@@ -22,11 +20,17 @@ const CourseContent = ({
     e.preventDefault();
   };
 
-  const handleCollapseToggle=(index)=>{
-    const updatedCollapsed=[...isCollapsed];
-    updatedCollapsed[index]= !updatedCollapsed[index];
+  const handleCollapseToggle = (index) => {
+    const updatedCollapsed = [...isCollapsed];
+    updatedData[index] = !updatedCollapsed[index];
     setIsCollapsed(updatedCollapsed);
-  }
+  };
+
+  const handleRemoveLink = (index, linkIndex) => {
+    const updatedData = [...courseContentData];
+    updatedData[index].links.splice(linkIndex, 1);
+    setCourseContentData(updatedData);
+  };
 
   return (
     <div className="w-[80%] m-auto mt-24 p-3">
@@ -42,24 +46,26 @@ const CourseContent = ({
                   showSectionInput ? "mt-10" : "mb-0"
                 }`}
               >
-
                 {showSectionInput && (
-                    <>
-                    <input type="text"
-                    className={`text-[20px] ${
-                        item.videoSection === "Untitled Section"
-                        ? "w-[170px]"
-                        : "w-min"
-                    } font-poppins cursor-pointer dark:text-white text-black bg-transparent outline-none`}
-                    value={item.videoSection}
-                    onChange={(e)=>{
-                        const updatedData=[...courseContentData];
-                        updatedData[index].videoSection=e.target.value;
-                        setCourseContentData(updatedData);
-                    }}
-                    />
-                    <BsPencil className="cursor-pointer dark:text-white text-black"/>
-                    </>
+                  <>
+                    <div className="w-full flex items-center">
+                      <input
+                        type="text"
+                        className={`text-[20px] ${
+                          item.videoSection === "Untitled Section"
+                            ? "w-[170px]"
+                            : "w-min"
+                        } font-poppins cursor-pointer dark:text-white text-black bg-transparent outline-none`}
+                        value={item.videoSection}
+                        onChange={(e) => {
+                          const updatedData = [...courseContentData];
+                          updatedData[index].videoSection = e.target.value;
+                          setCourseContentData(updatedData);
+                        }}
+                      />
+                      <BsPencil className="cursor-pointer dark:text-white text-black" />
+                    </div>
+                  </>
                 )}
                 <div className="flex w-full items-center justify-between my-0">
                   {isCollapsed[index] ? (
@@ -78,34 +84,102 @@ const CourseContent = ({
                   {/*arrow button for collapse video content  */}
                   <div className="flex items-center">
                     <AiOutlineDelete
-                    className={`dark:text-white text-[20px] mr-2 text-black ${index >0 ? "cursor-pointer": "cursor-no-drop"}`}
-                    onClick={()=> {
-                        if(index> 0){
-                            const updatedData=[...courseContentData];
-                            updatedData.splice(index,1);
-                            setCourseContentData(updatedData)
+                      className={`dark:text-white text-[20px] mr-2 text-black ${
+                        index > 0 ? "cursor-pointer" : "cursor-no-drop"
+                      }`}
+                      onClick={() => {
+                        if (index > 0) {
+                          const updatedData = [...courseContentData];
+                          updatedData.splice(index, 1);
+                          setCourseContentData(updatedData);
                         }
-                    }}
+                      }}
                     />
-                    <MdOutlineKeyboardArrowDown 
-                    fontSize="large"
-                    className="dark:text-white text-black"
-                    style={{
+                    <MdOutlineKeyboardArrowDown
+                      fontSize="large"
+                      className="dark:text-white text-black"
+                      style={{
                         transform: isCollapsed[index]
-                        ? "rotate(180deg)"
-                        : "rotate(0deg)"
-                    }}
-                    onClick={()=>handleCollapseToggle(index)}
+                          ? "rotate(180deg)"
+                          : "rotate(0deg)",
+                      }}
+                      onClick={() => handleCollapseToggle(index)}
                     />
-
                   </div>
                 </div>
                 {!isCollapsed[index] && (
-                    <>
+                  <>
                     <div className="my-3">
-<label className={`${styles.label}`}>Video Title</label>
+                      <label className={`${styles.label}`}>Video Title</label>
+                      <input
+                        className={`${styles.input}`}
+                        type="text"
+                        placeholder="Project plan...."
+                        value={item.title}
+                        onChange={(e) => {
+                          const updatedData = [...courseContentData];
+                          updatedData[index].title = e.target.value;
+                          setCourseContentData(updatedData);
+                        }}
+                      />
                     </div>
-                    </>
+                    <div className="my-3">
+                      <label className={`${styles.label}`}>Video Url</label>
+                      <input
+                        className={`${styles.input}`}
+                        type="text"
+                        placeholder="enter video url"
+                        value={item.videoUrl}
+                        onChange={(e) => {
+                          const updatedData = [...courseContentData];
+                          updatedData[index].videoUrl = e.target.value;
+                          setCourseContentData(updatedData);
+                        }}
+                      />
+                    </div>
+                    <div className="my-3">
+                      <label className={`${styles.label}`}>
+                        Video Description
+                      </label>
+                      <textarea
+                        rows={8}
+                        cols={30}
+                        className={`${styles.input} py-2`}
+                        placeholder="video description...."
+                        value={item.description}
+                        onChange={(e) => {
+                          const updatedData = [...courseContentData];
+                          updatedData[index].description = e.target.value;
+                          setCourseContentData(updatedData);
+                        }}
+                      />
+
+                      <br />
+                      <br />
+                      <br />
+                    </div>
+                    {item?.links.map((link, linkIndex) => (
+                      <div className="mb-3 block">
+                        <div className="w-full flex items-center justify-center">
+                          <label className={styles.label}>
+                            Link {linkIndex + 1}
+                          </label>
+                          <AiOutlineDelete
+                            className={`dark:text-white text-[20px] text-black ${
+                              linkIndex > 0
+                                ? "cursor-pointer"
+                                : "cursor-no-drop"
+                            }`}
+                            onClick={() => {
+                              linkIndex === 0
+                                ? null
+                                : handleRemoveLink(index, linkIndex);
+                            }}
+                          />
+                        </div>
+                      </div>
+                    ))}
+                  </>
                 )}
               </div>
             </>
